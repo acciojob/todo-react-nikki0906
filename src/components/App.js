8f137dd122d317e84bb6c4ed811fb4856669e109
Nikki Tiwari <nikkitiwari915@gmail.com>
2024-07-23 15:18:33 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,41 @@
-
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  }
+
+  const addTodo = () => {
+    if (inputValue.trim()) {
+      setTodos([...todos, inputValue]);
+      setInputValue("");
+    }
+  }
+
+  const deleteTodo = (todoToDelete) => {
+    const updatedTodos = todos.filter((todo) => todo !== todoToDelete);
+    setTodos(updatedTodos);
+  }
+  
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>To-Do List</h1>
+      <input value={inputValue} onChange={handleInputChange} />
+      <button onClick={addTodo}>Add Todo</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => deleteTodo(todo)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
